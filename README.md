4d47016ac97c3dbdd8387cb556179e32f2fc36a4
Ola Kleiven <Ola.Kleiven@nrk.no>
2021-03-07 20:48:29 +0100

    Fyrste versjon

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# nsb
+# Norske StorBand-arrangement
+
+## Sindre si superside som seier Storband som svimeslår sveisen, sann!
+
+## Installasjon
+
+Treng Node.js installert
+
+* git clone https://github.com/path/to/repo
+* npm ci
+
+## Oppdatere innhald
+
+* Gjer ønska endringer.
+* node generateSite.js - denne genererer index.html basert på index.ejs + data fra Google Sheets.
+* git commit -am "Fornuftig commit-melding"
+* git push
+* I løpet av nokre minutt vil da oppdatert side vere tilgjengeleg.
---
+++ generateSite.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const fetch = require('node-fetch')
+const ejs = require('ejs')
+
+//https://docs.google.com/spreadsheets/d/e/2PACX-1vQPUJx_XkBwdldi58FIIQdsNbRigmI7B7qbk9mmPeN0hyQ-dsFY23KRlpjhpHxnV4_QKzcGbGdwuxgl/pubhtml
+
+async function fetchRawData() {
+    const url = "https://spreadsheets.google.com/feeds/list/1AChhlsTROHI5fDE7ELmSTJdLZ_fy36leu7vk6OqbrrE/1/public/values?alt=json";
+
+    const response = await fetch(url);
+    const json = await response.json();
+
+    let data = [];
+
+    for (const row of json.feed.entry) {
+        const formattedRow = {}
+
+        for (const key in row) {
+            if (key.startsWith("gsx$")) {
+
+                formattedRow[key.replace("gsx$", "")] = row[key].$t
+
+            }
+        }
+
+        data.push(formattedRow)
+    }
+
+    return data;
+
+}
+
+async function generate() {
+
+    const data = await fetchRawData()
+
+    let tableRows = "";
+    for (const item of data) {
+        let row = "<tr>";
+        for (const [key, value] of Object.entries(item)) {
+            row += `<td>${value}</td>`
+        }
+        tableRows += `${row}</tr>\n`
+    }
+
+
+    const mainTableModel = {
+        content: tableRows
+    }
+    const html = await ejs.renderFile('index.ejs', { mainTable: mainTableModel }).then((output) => output)
+
+    fs.writeFileSync('index.html', html)
+
+}
+
+generate();
